db9034f92155dafa835279d27fa145b1130a501f
R2K-VENOM <84581636+R2K-VENOM@users.noreply.github.com>
2021-07-12 12:43:00 +0530

    Update alive.py

--- TelethonBot/plugins/alive.py
+++ TelethonBot/plugins/alive.py
@@ -6,11 +6,6 @@ async def alibe(event):
   SMEX_PIC = "https://telegra.ph/file/3979593187378b2b54057.jpg"
   but = [[Button.url("Mʏ ᴍᴀsᴛᴇʀ »»", "t.me/R2K_VENOM")]]
   pm_caption = """
-•**𝖨'𝖬 𝖠𝖫𝖨𝖵𝖤 𝖠𝖭𝖣 𝖱𝖤𝖠𝖣𝖸 𝖳𝖮 𝖲𝖤𝖬𝖷**\n
-•**Mʏ sʏsᴛᴇᴍ ɪs ᴘᴇʀғᴇᴄᴛʟʏ ʀᴜɴɴɪɴɢ**\n
-• Aʙᴏᴜᴛ ᴍʏ sʏsᴛᴇᴍ ✗\n
-• 𝙎𝙈𝙀𝙓 𝙓 𝙑𝙀𝙍𝙎𝙄𝙊𝙉: 1.1\n
-• 𝙊𝙒𝙉𝙀𝙍 ☞ [𓆩𝙑𝙀𝙉𝙊𝙈𓆪](t.me/R2K_VENOM)\n
-
+•♦• **𝖨'𝖬 𝖠𝖫𝖨𝖵𝖤 𝖠𝖭𝖣 𝖱𝖤𝖠𝖣𝖸 𝖳𝖮 𝖲𝖤𝖬𝖷**
 """
   await BotzHub.send_file(event.chat_id, file=SMEX_PIC, caption=pm_caption, buttons=but, link_preview=False)
