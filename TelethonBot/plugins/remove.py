19a3fbb51bad450953b6f5ab658d9c011a5721b8
R2K-VENOM <84581636+R2K-VENOM@users.noreply.github.com>
2021-07-12 08:47:53 +0530

    Update remove.py

--- TelethonBot/plugins/remove.py
+++ TelethonBot/plugins/remove.py
@@ -1,3 +1,13 @@
 from .. import BotzHub
 from telethon import events, Button
 
+SMEX_USER = [1809900087]
+
+@BotzHub.on(
+    events.NewMessage(pattern="^/remove ?(.*)")
+)
+async def _(event):
+  if event.sender_id in SMEX_USER:
+    await bot.send_message(event.chat_id, "SEMX DONE NOW TAKE REST", buttons=Button.clear())
+  else:
+    await event.reply("**BHAI YAAR THUM GAAND MARAO**")
