5466c8f8e49dca372ea62f93268d333ba3e6709d
R2K-VENOM <84581636+R2K-VENOM@users.noreply.github.com>
2021-07-12 11:35:19 +0530

    Update alive.py

--- TelethonBot/plugins/alive.py
+++ TelethonBot/plugins/alive.py
@@ -5,13 +5,14 @@ from telethon import events, Button
 async def alibe(event):
   SMEX_PIC = "https://telegra.ph/file/3979593187378b2b54057.jpg"
   but = [[Button.url("Mʏ ᴍᴀsᴛᴇʀ »»", "t.me/R2K_VENOM")]]
-  pm_caption = "•**I'M ALIVE AND READY TO SMEX**\n\n"
-  pm_caption += "•**Mʏ sʏsᴛᴇᴍ ɪs ᴘᴇʀғᴇᴄᴛʟʏ ʀᴜɴɴɪɴɢ**\n\n"
-  pm_caption += "• Aʙᴏᴜᴛ ᴍʏ sʏsᴛᴇᴍ ✗\n\n"
-  pm_caption += "• 𝙎𝙈𝙀𝙓 𝙓 𝙑𝙀𝙍𝙎𝙄𝙊𝙉: 1.1\n"
-  pm_caption += "• 𝙏𝙀𝙇𝙀𝙏𝙃𝙊𝙉 𝙑𝙀𝙍𝙎𝙄𝙊𝙉 ☞ {version.__version__}\n"
-  pm_caption += (
-        "• 𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏 𝘽𝙔 ☞ [𓆩𝙑𝙀𝙉𝙊𝙈𓆪](t.me/R2K_VENOM)\n\n"
+  pm_caption = """
+•**I'M ALIVE AND READY TO SMEX**\n\n
+•**Mʏ sʏsᴛᴇᴍ ɪs ᴘᴇʀғᴇᴄᴛʟʏ ʀᴜɴɴɪɴɢ**\n\n
+• Aʙᴏᴜᴛ ᴍʏ sʏsᴛᴇᴍ ✗\n\n
+• 𝙎𝙈𝙀𝙓 𝙓 𝙑𝙀𝙍𝙎𝙄𝙊𝙉: 1.1\n• 𝙏𝙀𝙇𝙀𝙏𝙃𝙊𝙉 𝙑𝙀𝙍𝙎𝙄𝙊𝙉 ☞ {version.__version__}\n
+(• 𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏 𝘽𝙔 ☞ [𓆩𝙑𝙀𝙉𝙊𝙈𓆪](t.me/R2K_VENOM)\n\n
     )
-  pm_caption += f"• 𝙊𝙒𝙉𝙀𝙍 ☞ [𓆩𝙑𝙀𝙉𝙊𝙈𓆪](t.me/R2K_VENOM)\n"
+• 𝙊𝙒𝙉𝙀𝙍 ☞ [𓆩𝙑𝙀𝙉𝙊𝙈𓆪](t.me/R2K_VENOM)\n
+
+"""
   await BotzHub.send_file(event.chat_id, file=SMEX_PIC, captions=pm_caption, buttons=but, link_preview=False)
