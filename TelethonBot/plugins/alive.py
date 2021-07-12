84f6f0e68476606efb90103c5a0883374ad904ec
R2K-VENOM <84581636+R2K-VENOM@users.noreply.github.com>
2021-07-12 12:32:18 +0530

    Update alive.py

--- TelethonBot/plugins/alive.py
+++ TelethonBot/plugins/alive.py
@@ -6,12 +6,10 @@ async def alibe(event):
   SMEX_PIC = "https://telegra.ph/file/3979593187378b2b54057.jpg"
   but = [[Button.url("Mʏ ᴍᴀsᴛᴇʀ »»", "t.me/R2K_VENOM")]]
   pm_caption = """
-•**I'M ALIVE AND READY TO SMEX**\n\n
-•**Mʏ sʏsᴛᴇᴍ ɪs ᴘᴇʀғᴇᴄᴛʟʏ ʀᴜɴɴɪɴɢ**\n\n
-• Aʙᴏᴜᴛ ᴍʏ sʏsᴛᴇᴍ ✗\n\n
-• 𝙎𝙈𝙀𝙓 𝙓 𝙑𝙀𝙍𝙎𝙄𝙊𝙉: 1.1\n• 𝙏𝙀𝙇𝙀𝙏𝙃𝙊𝙉 𝙑𝙀𝙍𝙎𝙄𝙊𝙉 ☞ {version.__version__}\n
-(• 𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏 𝘽𝙔 ☞ [𓆩𝙑𝙀𝙉𝙊𝙈𓆪](t.me/R2K_VENOM)\n\n
-    )
+•**𝖨'𝖬 𝖠𝖫𝖨𝖵𝖤 𝖠𝖭𝖣 𝖱𝖤𝖠𝖣𝖸 𝖳𝖮 𝖲𝖤𝖬𝖷**\n
+•**Mʏ sʏsᴛᴇᴍ ɪs ᴘᴇʀғᴇᴄᴛʟʏ ʀᴜɴɴɪɴɢ**\n
+• Aʙᴏᴜᴛ ᴍʏ sʏsᴛᴇᴍ ✗\n
+• 𝙎𝙈𝙀𝙓 𝙓 𝙑𝙀𝙍𝙎𝙄𝙊𝙉: 1.1\n
 • 𝙊𝙒𝙉𝙀𝙍 ☞ [𓆩𝙑𝙀𝙉𝙊𝙈𓆪](t.me/R2K_VENOM)\n
 
 """
